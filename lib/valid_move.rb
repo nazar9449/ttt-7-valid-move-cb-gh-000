4f7bf0708dba6494844d13b09bbaaef569b0c733
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 03:39:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,6 +16,6 @@ def position_taken?(board=[" "," "," "," "," "," "," "," "," "], index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-/home/redolent-architecture-9667/ttt-7-valid-move-cb-gh-000/spec/valid_move_spec.rb:1:in `require_relative': 
-/home/redolent-architecture-9667/ttt-7-valid-move-cb-gh-000/lib/valid_move.rb:17: syntax error, 
+/home/redolent-architecture-9667/ttt-7-valid-move-cb-gh-000/spec/valid_move_spec.rb:1:in `require_relative':
+/home/redolent-architecture-9667/ttt-7-valid-move-cb-gh-000/lib/valid_move.rb:17: syntax error,
 unexpected end-of-input, expecting end (SyntaxError)
