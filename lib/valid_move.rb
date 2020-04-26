7c112f7a2836da34e5409e7fa1ad3133224c01d3
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 03:31:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board=[" "," "," "," "," "," "," "," "," "], index)
+  
 end
 
 def position_taken?(board=[" "," "," "," "," "," "," "," "," "], index)
